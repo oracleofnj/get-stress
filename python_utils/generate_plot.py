eccbbde92d72b911b8b0038de5f94b87bd9196a2
oracleofnj <jared.samet@aya.yale.edu>
2017-12-20 13:56:48 -0500

    Agggg

--- python_utils/generate_plot.py
+++ python_utils/generate_plot.py
@@ -1,14 +1,13 @@
 from __future__ import print_function
 from __future__ import division
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib
 import json
 import pickle
 import os
 import sys
 from clustering_utils_new import plot_utterances
 
-import matplotlib
 matplotlib.use('Agg')
 
 if __name__ == "__main__":
